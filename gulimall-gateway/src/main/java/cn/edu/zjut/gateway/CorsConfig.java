e95cbbb23969246421f3495f5427744f60fb5fa7
zett0n <d673326004@163.com>
2021-08-26 02:52:24 +0800

    完成p47，将renren-fast引入工程并导入cmmon依赖，修改renren-fast部分代码

---
+++ gulimall-gateway/src/main/java/cn/edu/zjut/gateway/CorsConfig.java
@@ -0,0 +1,37 @@
+package cn.edu.zjut.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GulimallCorsConfiguration {
+
+    // 设置跨域过滤器
+    // 产生这个Bean对象交给容器管理
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+
+        // CorsConfiguration 类代表跨域配置信息
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // 配置跨域相关信息
+        // 允许哪些请求头跨域
+        corsConfiguration.addAllowedHeader("*");
+        // 允许哪些请求方式跨域（get,put...)
+        corsConfiguration.addAllowedMethod("*");
+        // 允许哪些请求来源跨域
+        corsConfiguration.addAllowedOrigin("*");
+        // 允许携带cookie跨域
+        corsConfiguration.setAllowCredentials(true);
+
+        // 对所有路径，注册上文的跨域配置
+        configSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        // CorsWebFilter 需要传 CorsConfigurationSource，也就是跨域配置源
+        return new CorsWebFilter(configSource);
+    }
+}
